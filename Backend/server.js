11bd96973f95cdaedb6d02bb1cbbbd944347dbfd
Mukesh Swain <161455068+MukeshSwain@users.noreply.github.com>
2025-04-18 22:55:40 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -20,6 +20,10 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running" });
+});
+
 
 app.use("/api/user", userRoute);
 app.use("/api/quiz", quizRoute);
